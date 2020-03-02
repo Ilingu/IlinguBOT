09176dcdeb9f3d2201374fcfec6a1e1326514612
TheTitou <thetitouoff@gmail.com>
2020-03-02 23:42:05 +0100

    + cmd ban/kick

--- index.js
+++ index.js
@@ -45,28 +45,29 @@ client.on("message", async message => {
 
     if (ping <= 50) {
       msg.edit(
-        `🏓 Pong\nTon ping client est de ${Math.floor(
+        `🏓 Pong\n✔✔Ping client ${Math.floor(
           msg.createdAt - message.createdAt
-        )}\nTon ping server (ton vrai ping) est de ${ping}ms => Ta un très bon ping`
+        )}\n✔✔Ton ping est donc de ${ping}ms soit ${ping /
+          1000}s => Ta un très bon ping`
       );
     } else if (ping > 50 && ping <= 100) {
       msg.edit(
-        `🏓 Pong\nTon ping client est de ${Math.floor(
+        `🏓 Pong\n✔Ping client ${Math.floor(
           msg.createdAt - message.createdAt
-        )}\nTon ping server (ton vrai ping) est de ${ping}ms => Ta un bon ping`
+        )}\n✔Ton ping est donc de ${ping}ms soit ${ping /
+          1000}s => Ta un bon ping`
       );
     } else if (ping > 100 && ping <= 350) {
       msg.edit(
-        `🏓 Pong\nTon ping client est de ${Math.floor(
+        `🏓 Pong\n🆗Ping client ${Math.floor(
           msg.createdAt - message.createdAt
-        )}\nTon ping server (ton vrai ping) est de ${ping}ms => Ton ping est moyen`
+        )}\n🆗Ton ping est donc de ${ping}ms soit ${ping /
+          1000}s => Ton ping est moyen`
       );
     } else {
       msg.edit(
-        `🏓 Pong\n A mais là c'est pas un ping que ta ptn : ${ping}ms\n, c'est à dire que tes amis t'entendent ${ping /
-          1000}s après que tu ait parlé\n, non vraiment arrête de jouer c'est un masacre !\nPing client : ${Math.floor(
-          msg.createdAt - message.createdAt
-        )}`
+        `🏓 Pong\n ❌ ouïe ça doit piquer ton ping est de ${ping}ms\n soit ${ping /
+          1000}s ❌ pour faire une requête et que la réponse te parvienne 😢😢😢`
       );
     }
   } else if (cmd === "say") {
@@ -145,18 +146,200 @@ client.on("message", async message => {
       .setTitle(`From r/${random} (Reddit)`)
       .setURL(`https://reddit.com/r/${random}`);
     message.channel.send(embed);
-  } else if (cmd === "niggerCovid") {
+  } else if (cmd === "kick") {
+    const logChannel =
+      message.guild.channels.find(c => c.name === "logs") || message.channel;
+
+    if (message.deletable) message.delete();
+
+    // No Args
+    if (!args[0]) {
+      return message
+        .reply("Please provide a reason to kick")
+        .then(m => m.delete(5000));
+    }
+    // No reason
+    if (!args[1]) {
+      return message
+        .reply("Please provide a reason to kick")
+        .then(m => m.delete(5000));
+    }
+    // No author permissons
+    if (!message.member.hasPermission("KICK_MEMBERS")) {
+      return message
+        .reply(
+          "❌ You do not have permissions to kick members. Please contact a staff member ❌"
+        )
+        .then(m => m.delete(5000));
+    }
+    // No bot permissions
+    if (!message.guild.me.hasPermission("KICK_MEMBERS")) {
+      return message
+        .reply(
+          "❌ This bot haven't permissions to kick members please upgrade this grade into this server if you want he does this. ❌"
+        )
+        .then(m => m.delete(5000));
+    }
+
+    const toKick =
+      message.mentions.members.first() || message.guild.members.get(args[0]);
+
+    // No member found
+    if (!toKick) {
+      return message
+        .reply("Couldn't find that member, try again!")
+        .then(m => m.delete(5000));
+    }
+    // Can't kick urself
+    if (message.author.id === toKick.id) {
+      return message
+        .reply("Can't kick yourself, smartboii")
+        .then(m => m.delete(5000));
+    }
+    // Kickable
+    if (!toKick.kickable) {
+      return message
+        .reply("I can't kick that person due to role hietatchy, I suppose.")
+        .then(m => m.delete(5000));
+    }
+    if (toKick === "@Ilingu") {
+      return message
+        .reply(
+          "You couldn't kick my creators now I ban u (no that a fucking joke because you can't kick me or kick the bot of you're server hahaha)"
+        )
+        .then(m => m.delete(5000));
+    }
     const embed = new RichEmbed()
-      .setColor("RANDOM")
-      .setImage(
-        "https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"
-      )
-      .setTitle("Avancer du coronavirus")
-      .setURL(
-        "https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"
+      .setColor("#ff0000")
+      .setThumbnail(toKick.user.displayAvatarURL)
+      .setFooter(message.member.displayName, message.author.displayAvatarURL)
+      .setTimestamp()
+      .setDescription(
+        `❌ Kicked member: **${toKick}** ❌\nKicked by : **${
+          message.author
+        }** (${message.author.id})✔\nReason : **${args.slice(1).join(" ")}**`
       );
 
-    message.channel.send(embed);
+    const promptEmbed = new RichEmbed()
+      .setColor("GREEN")
+      .setAuthor("This verification becomes invalid after 30s")
+      .setDescription(`Do you want to kick ${toKick}`);
+
+    message.channel.send(promptEmbed).then(async msg => {
+      const emoji = await promptMessage(msg, message.author, 30, ["✅", "❌"]);
+
+      if (emoji === "✅") {
+        msg.delete();
+
+        toKick.kick(args.slice(1).join(" ")).catch(err => {
+          if (err)
+            return message.channel.send(`Well..... Something went wrong?`);
+        });
+
+        logChannel.send(embed);
+      } else if (emoji === "❌") {
+        msg.delete();
+
+        message.reply("Kick canceled...").then(m => m.delete(5000));
+      }
+    });
+  } else if (cmd === "ban") {
+    const logChannel =
+      message.guild.channels.find(c => c.name === "logs") || message.channel;
+
+    if (message.deletable) message.delete();
+
+    // No Args
+    if (!args[0]) {
+      return message
+        .reply("Please provide a reason to ban")
+        .then(m => m.delete(5000));
+    }
+    // No reason
+    if (!args[1]) {
+      return message
+        .reply("Please provide a reason to ban")
+        .then(m => m.delete(5000));
+    }
+    // No author permissons
+    if (!message.member.hasPermission("BAN_MEMBERS")) {
+      return message
+        .reply(
+          "❌ You do not have permissions to ban members. Please contact a staff member ❌"
+        )
+        .then(m => m.delete(5000));
+    }
+    // No bot permissions
+    if (!message.guild.me.hasPermission("BAN_MEMBERS")) {
+      return message
+        .reply(
+          "❌ This bot haven't permissions to ban members please upgrade this grade into this server if you want he does this. ❌"
+        )
+        .then(m => m.delete(5000));
+    }
+
+    const toBan =
+      message.mentions.members.first() || message.guild.members.get(args[0]);
+
+    // No member found
+    if (!toBan) {
+      return message
+        .reply("Couldn't find that member, try again!")
+        .then(m => m.delete(5000));
+    }
+    // Can't kick urself
+    if (message.author.id === toBan.id) {
+      return message
+        .reply("Can't ban yourself, smartboii")
+        .then(m => m.delete(5000));
+    }
+    // Kickable
+    if (!toBan.kickable) {
+      return message
+        .reply("I can't ban that person due to role hietatchy, I suppose.")
+        .then(m => m.delete(5000));
+    }
+    if (toBan === "@Ilingu") {
+      return message
+        .reply(
+          "You couldn't ban my creators now I ban u (no that a fucking joke because you can't ban me or ban the bot of you're server hahaha)"
+        )
+        .then(m => m.delete(5000));
+    }
+    const embed = new RichEmbed()
+      .setColor("#ff0000")
+      .setThumbnail(toKick.user.displayAvatarURL)
+      .setFooter(message.member.displayName, message.author.displayAvatarURL)
+      .setTimestamp()
+      .setDescription(
+        `❌ Baned member: **${toKick}** ❌\nBaned by : **${message.author}** (${
+          message.author.id
+        })✔\nReason : **${args.slice(1).join(" ")}**`
+      );
+
+    const promptEmbed = new RichEmbed()
+      .setColor("GREEN")
+      .setAuthor("This verification becomes invalid after 30s")
+      .setDescription(`Do you want to ban ${toKick}`);
+
+    message.channel.send(promptEmbed).then(async msg => {
+      const emoji = await promptMessage(msg, message.author, 30, ["✅", "❌"]);
+
+      if (emoji === "✅") {
+        msg.delete();
+
+        toKick.kick(args.slice(1).join(" ")).catch(err => {
+          if (err)
+            return message.channel.send(`Well..... Something went wrong?`);
+        });
+
+        logChannel.send(embed);
+      } else if (emoji === "❌") {
+        msg.delete();
+
+        message.reply("Ban canceled...").then(m => m.delete(5000));
+      }
+    });
   } else {
     if (message.deletable) message.delete();
   }
