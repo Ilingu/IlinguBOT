52917cd5d1cf88eb19f94e793687a2c7774d480b
TheTitou <thetitouoff@gmail.com>
2020-04-29 11:43:11 +0200

    +U gay->No U

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const randomPuppy = require("random-puppy");
 const chooseArr = ["⛰", "🧻", "✂"];
 
 const client = new Client({
-  disableEveryone: true
+  disableEveryone: true,
 });
 
 config({
-  path: __dirname + "/.env"
+  path: __dirname + "/.env",
 });
 
 client.on("ready", () => {
@@ -20,22 +20,29 @@ client.on("ready", () => {
     status: "online",
     game: {
       name: "Tes ambitions",
-      type: "LISTENING"
-    }
+      type: "LISTENING",
+    },
   });
 });
 
-client.on("message", async message => {
+client.on("message", async (message) => {
   const prefix = "_";
 
   if (message.author.bot) return;
   if (!message.guild) return;
-  if (!message.content.startsWith(prefix)) return;
+  if (!message.content.startsWith(prefix)) {
+    if (
+      message.content.toLowerCase() === "ugay" ||
+      message.content.toLowerCase() === "u gay"
+    ) {
+      message.channel.send("No u");
+      return;
+    } else {
+      return;
+    }
+  }
 
-  const args = message.content
-    .slice(prefix.length)
-    .trim()
-    .split(/ +/g);
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
 
   if (cmd === "ping") {
@@ -47,34 +54,38 @@ client.on("message", async message => {
       msg.edit(
         `🏓 Pong\n✔✔Ping client ${Math.floor(
           msg.createdAt - message.createdAt
-        )}\n✔✔Ton ping est donc de ${ping}ms soit ${ping /
-          1000}s => Ta un très bon ping`
+        )}\n✔✔Ton ping est donc de ${ping}ms soit ${
+          ping / 1000
+        }s => Ta un très bon ping`
       );
     } else if (ping > 50 && ping <= 100) {
       msg.edit(
         `🏓 Pong\n✔Ping client ${Math.floor(
           msg.createdAt - message.createdAt
-        )}\n✔Ton ping est donc de ${ping}ms soit ${ping /
-          1000}s => Ta un bon ping`
+        )}\n✔Ton ping est donc de ${ping}ms soit ${
+          ping / 1000
+        }s => Ta un bon ping`
       );
     } else if (ping > 100 && ping <= 350) {
       msg.edit(
         `🏓 Pong\n🆗Ping client ${Math.floor(
           msg.createdAt - message.createdAt
-        )}\n🆗Ton ping est donc de ${ping}ms soit ${ping /
-          1000}s => Ton ping est moyen`
+        )}\n🆗Ton ping est donc de ${ping}ms soit ${
+          ping / 1000
+        }s => Ton ping est moyen`
       );
     } else {
       msg.edit(
-        `🏓 Pong\n ❌ ouïe ça doit piquer ton ping est de ${ping}ms\n soit ${ping /
-          1000}s ❌ pour faire une requête et que la réponse te parvienne 😢😢😢`
+        `🏓 Pong\n ❌ ouïe ça doit piquer ton ping est de ${ping}ms\n soit ${
+          ping / 1000
+        }s ❌ pour faire une requête et que la réponse te parvienne 😢😢😢`
       );
     }
   } else if (cmd === "say") {
     if (message.deletable) message.delete();
 
     if (args.length < 1)
-      return message.reply("Nothings to say ?").then(m => m.delete(5000));
+      return message.reply("Nothings to say ?").then((m) => m.delete(5000));
 
     const roleColor = message.guild.me.displayHexColor;
 
@@ -148,7 +159,7 @@ client.on("message", async message => {
     message.channel.send(embed);
   } else if (cmd === "kick") {
     const logChannel =
-      message.guild.channels.find(c => c.name === "logs") || message.channel;
+      message.guild.channels.find((c) => c.name === "logs") || message.channel;
 
     if (message.deletable) message.delete();
 
@@ -156,13 +167,13 @@ client.on("message", async message => {
     if (!args[0]) {
       return message
         .reply("Please provide a reason to kick")
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
     // No reason
     if (!args[1]) {
       return message
         .reply("Please provide a reason to kick")
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
     // No author permissons
     if (!message.member.hasPermission("KICK_MEMBERS")) {
@@ -170,7 +181,7 @@ client.on("message", async message => {
         .reply(
           "❌ You do not have permissions to kick members. Please contact a staff member ❌"
         )
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
     // No bot permissions
     if (!message.guild.me.hasPermission("KICK_MEMBERS")) {
@@ -178,7 +189,7 @@ client.on("message", async message => {
         .reply(
           "❌ This bot haven't permissions to kick members please upgrade this grade into this server if you want he does this. ❌"
         )
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
 
     const toKick =
@@ -188,26 +199,26 @@ client.on("message", async message => {
     if (!toKick) {
       return message
         .reply("Couldn't find that member, try again!")
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
     // Can't kick urself
     if (message.author.id === toKick.id) {
       return message
         .reply("Can't kick yourself, smartboii")
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
     // Kickable
     if (!toKick.kickable) {
       return message
         .reply("I can't kick that person due to role hietatchy, I suppose.")
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
     if (toKick === "@Ilingu") {
       return message
         .reply(
           "You couldn't kick my creators now I ban u (no that a fucking joke because you can't kick me or kick the bot of you're server hahaha)"
         )
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
     const embed = new RichEmbed()
       .setColor("#ff0000")
@@ -225,13 +236,13 @@ client.on("message", async message => {
       .setAuthor("This verification becomes invalid after 30s")
       .setDescription(`Do you want to kick ${toKick}`);
 
-    message.channel.send(promptEmbed).then(async msg => {
+    message.channel.send(promptEmbed).then(async (msg) => {
       const emoji = await promptMessage(msg, message.author, 30, ["✅", "❌"]);
 
       if (emoji === "✅") {
         msg.delete();
 
-        toKick.kick(args.slice(1).join(" ")).catch(err => {
+        toKick.kick(args.slice(1).join(" ")).catch((err) => {
           if (err)
             return message.channel.send(`Well..... Something went wrong?`);
         });
@@ -240,12 +251,12 @@ client.on("message", async message => {
       } else if (emoji === "❌") {
         msg.delete();
 
-        message.reply("Kick canceled...").then(m => m.delete(5000));
+        message.reply("Kick canceled...").then((m) => m.delete(5000));
       }
     });
   } else if (cmd === "ban") {
     const logChannel =
-      message.guild.channels.find(c => c.name === "logs") || message.channel;
+      message.guild.channels.find((c) => c.name === "logs") || message.channel;
 
     if (message.deletable) message.delete();
 
@@ -253,13 +264,13 @@ client.on("message", async message => {
     if (!args[0]) {
       return message
         .reply("Please provide a reason to ban")
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
     // No reason
     if (!args[1]) {
       return message
         .reply("Please provide a reason to ban")
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
     // No author permissons
     if (!message.member.hasPermission("BAN_MEMBERS")) {
@@ -267,7 +278,7 @@ client.on("message", async message => {
         .reply(
           "❌ You do not have permissions to ban members. Please contact a staff member ❌"
         )
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
     // No bot permissions
     if (!message.guild.me.hasPermission("BAN_MEMBERS")) {
@@ -275,7 +286,7 @@ client.on("message", async message => {
         .reply(
           "❌ This bot haven't permissions to ban members please upgrade this grade into this server if you want he does this. ❌"
         )
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
 
     const toBan =
@@ -285,26 +296,26 @@ client.on("message", async message => {
     if (!toBan) {
       return message
         .reply("Couldn't find that member, try again!")
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
     // Can't kick urself
     if (message.author.id === toBan.id) {
       return message
         .reply("Can't ban yourself, smartboii")
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
     // Kickable
     if (!toBan.kickable) {
       return message
         .reply("I can't ban that person due to role hietatchy, I suppose.")
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
     if (toBan === "@Ilingu") {
       return message
         .reply(
           "You couldn't ban my creators now I ban u (no that a fucking joke because you can't ban me or ban the bot of you're server hahaha)"
         )
-        .then(m => m.delete(5000));
+        .then((m) => m.delete(5000));
     }
     const embed = new RichEmbed()
       .setColor("#ff0000")
@@ -322,13 +333,13 @@ client.on("message", async message => {
       .setAuthor("This verification becomes invalid after 30s")
       .setDescription(`Do you want to ban ${toKick}`);
 
-    message.channel.send(promptEmbed).then(async msg => {
+    message.channel.send(promptEmbed).then(async (msg) => {
       const emoji = await promptMessage(msg, message.author, 30, ["✅", "❌"]);
 
       if (emoji === "✅") {
         msg.delete();
 
-        toKick.kick(args.slice(1).join(" ")).catch(err => {
+        toKick.kick(args.slice(1).join(" ")).catch((err) => {
           if (err)
             return message.channel.send(`Well..... Something went wrong?`);
         });
@@ -337,7 +348,7 @@ client.on("message", async message => {
       } else if (emoji === "❌") {
         msg.delete();
 
-        message.reply("Ban canceled...").then(m => m.delete(5000));
+        message.reply("Ban canceled...").then((m) => m.delete(5000));
       }
     });
   } else {
