56ae8a96679802e12e9a764242367504175ec45f
Ilingu <thetitouoff@gmail.com>
2021-04-05 20:14:35 +0200

    refix bug

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ const LevelUp = (User, guild, Data) => {
     AllData[User].xp = 0;
     AllData[User].lvl += 1;
     const channel = client.channels.cache.find((ch) => ch.name === "annonces");
-    const EmojiGG = message.emojis.cache.find((emoji) => emoji.name == "GG");
+    const EmojiGG = client.emojis.cache.find((emoji) => emoji.name == "GG");
     channel.send(
       `✅ <:${EmojiGG.name}:${EmojiGG.id}> ${
         client.users.cache.find((us) => us.id === User).username
