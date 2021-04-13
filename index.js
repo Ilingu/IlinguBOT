c70c07b63cef80b16c791c0fa13928898e7bd5ef
Ilingu <thetitouoff@gmail.com>
2021-04-13 20:58:49 +0200

    Fix bug

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ const LevelUp = (User, guild, Data, MessageLength) => {
   // If last user message is under 1min -> return
   if (
     AllData[User].LastMessageTime &&
-    Date.now() - AllData[User].LastMessageTime >= 60000
+    Date.now() - AllData[User].LastMessageTime < 60000
   )
     return;
   // ADD
